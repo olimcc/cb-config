0775b3b66393ad1a0c431afbc451b6b374f916b8
Oli McCormack <oli@tothelee.co>
2022-10-06 17:46:18 -0700

    init commit

---
+++ test.py
@@ -0,0 +1,25 @@
+import yaml
+import unittest
+from dataclasses import dataclass
+
+class TestYAMLContents(unittest.TestCase):
+
+    def test_YAML(self):
+        with open('config.yaml') as ymlfile:
+            contents = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            self.assertIn('targets', contents)
+            for entry in contents['targets']:
+                required_keys = [
+                    'api', 'place', 'placeName', 'campground',
+                    'day', 'nights', 'months', 'telegramChats']
+                for k in required_keys:
+                    self.assertIn(k, entry)
+                if 'ranges' in entry:
+                    for range in entry['ranges']:
+                        for k in ['startDate', 'endDate']:
+                            self.assertIn(k, range, f'Full erroneous object:\n{yaml.dump(entry)}')
+                if 'sites' in entry:
+                    self.assertTrue(isinstance(entry['sites'], list))
+
+if __name__ == '__main__':
+    unittest.main()
